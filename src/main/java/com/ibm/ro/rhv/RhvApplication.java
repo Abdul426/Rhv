9f232ca3e474cdaf115a6648baac545299c3fba1
Abdul426 <munnaf426@gmail.com>
2020-11-02 15:00:09 +0530

    Initial Commit

---
+++ src/main/java/com/ibm/ro/rhv/RhvApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.ro.rhv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhvApplication.class, args);
+	}
+
+}
