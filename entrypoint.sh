ebda18167ddb41111e15f2b8b680b0cb71c165cb
Abdul426 <munnaf426@gmail.com>
2021-02-05 21:09:32 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,8 @@
 #    if [ "$DEBUG_SUSPEND" == "true" ]; then
 #        export JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000"
 #    else
-		echo "Setting JAVA_OPTS"
-        export JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000"
-		echo "JAVA_OPTS: " $JAVA_OPTS
-		
-		echo "Testing java"
-		sh java
-		
+		#The thing to note is the 3rd line where I’m setting JAVA_TOOL_OPTIONS environment variable. This variable tells the JRE that it has to enable JPDA session so that the application can be debugged remotely using Java Debug Wire Protocol (JWDP).
+		export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n"
 		
 #    fi
 #fi
